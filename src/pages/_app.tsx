2366fd144c3b4d208ad309adb92a3d8abfe4ed19
Ignazio Margiotta <hello@ignaziomargiotta.dev>
2024-03-05 10:19:01 +0100

    add google tag mananger

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,7 @@
+import { useEffect } from "react";
 import type { AppProps } from "next/app";
 import { ApolloProvider } from "@apollo/client";
+import TagManager from 'react-gtm-module';
 
 // Styles
 import "@theartcode/styles/globals.scss";
@@ -8,6 +10,13 @@ import "@theartcode/styles/globals.scss";
 import client from "@theartcode/api/apollo-client";
 
 export default function App({ Component, pageProps }: AppProps) {
+
+  useEffect(()=>{
+    TagManager.initialize({
+      gtmId: 'G-PXKMHSTBF9'
+    })
+  },[]);
+
   return (
   <ApolloProvider client={client}>
     <Component {...pageProps} />
