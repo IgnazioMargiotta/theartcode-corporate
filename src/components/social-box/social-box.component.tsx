92eafedb9ce6d6447ce5ad0b606172282ed303be
Ignazio Margiotta <hello@ignaziomargiotta.dev>
2024-03-07 15:21:33 +0100

    add socials link

--- src/components/social-box/social-box.component.tsx
+++ src/components/social-box/social-box.component.tsx
@@ -8,12 +8,12 @@ interface SocialBoxProps {
 export const SocialBox = ({ size = 'medium' } : SocialBoxProps) => {
   return (
     <div className={`${styles['social']}`} data-size={size}>
-      <Link className={`${styles['social__instagram']}`} href="#" target="_blank" title="Instagram">
+      <Link className={`${styles['social__instagram']}`} href="https://www.instagram.com/theart.code/" target="_blank" title="Instagram">
         <svg viewBox="0 0 30 30">
           <use href="#social-instagram"></use>
         </svg>
       </Link>
-      <Link className={`${styles['social__linkedin']}`} href="#" target="_blank" title="LinkedIn">
+      <Link className={`${styles['social__linkedin']}`} href="https://www.linkedin.com/company/vibra-studio/" target="_blank" title="LinkedIn">
         <svg viewBox="0 0 26 26">
           <use href="#social-linkedin"></use>
         </svg>
