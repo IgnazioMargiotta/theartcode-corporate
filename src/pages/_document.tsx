84b4a2d3d0e32a275ccf2d73374babe0fe315b47
Ignazio Margiotta <hello@ignaziomargiotta.dev>
2024-03-12 14:42:32 +0100

    fix site lang

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,15 +1,22 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import Document, { Html, Head, Main, NextScript } from 'next/document';
 
-export default function Document() {
-  return (
-    <Html lang="en">
-      <Head>
-        <link rel="icon" type="image/png" href="/assets/images/favicon.png" />
-      </Head>
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  );
+class MyDocument extends Document {
+
+  render() {
+    const lang = this.props.__NEXT_DATA__.page.startsWith('/it') ? 'it' : 'en';
+
+    return (
+      <Html lang={lang}>
+        <Head>
+          <link rel="icon" type="image/png" href="/assets/images/favicon.png" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
+
+export default MyDocument;
