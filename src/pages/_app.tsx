97a9140779a1aba0b66ce5d785855090f80c4e7f
Ignazio Margiotta <hello@ignaziomargiotta.dev>
2024-03-26 09:59:25 +0100

    update app google scripts

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,14 +7,13 @@ import "../styles/globals.scss";
 // Utils
 import client from "@theartcode/api/apollo-client";
 import { ThemeProvider } from "../contexts/ThemeContext";
-import { IubendaScript, GoogleAdsScript } from "@theartcode/components";
+import { IubendaScript } from "@theartcode/components";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ApolloProvider client={client}>
       <ThemeProvider>
         <IubendaScript />
-        <GoogleAdsScript />
         <Component {...pageProps} />
       </ThemeProvider>
     </ApolloProvider>
